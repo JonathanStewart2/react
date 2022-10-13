7eb99374809f149d339bc7989a2a3dc7369745c5
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-10-13 16:56:19 +0100

    updating

--- demo/src/Components/States/Converter.jsx
+++ demo/src/Components/States/Converter.jsx
@@ -2,33 +2,27 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const Converter = () => {
-    const [value, setValue] = useState(0)
-    let currentBTC = 0
+    const [value, setValue] = useState(0);
+    let [btc, setBtc] = useState(0);
 
     const changeHandler = (event) => {
-        setValue(event.target.value);
+        setValue(parseFloat(event.target.value));
     }
 
-    const convert2BTC = () => {
+    const convert2BTC = async () => {
         const btcURL = "https://api.coindesk.com/v1/bpi/currentprice.json";
-        axios.get(btcURL)
-            .then(response => {
-                currentBTC = parseInt(response.data.bpi.GBP.rate);
-                //console.log(response.data.bpi.GBP.rate);
-                console.log(currentBTC);
-                console.log(currentBTC/value);
-            })
-            .catch(err => console.error(err))
-        return currentBTC/value;
+        const response = await axios.get(btcURL)
+
+        setBtc(value / response.data.bpi.GBP.rate_float);
     }
 
     return (
         <>
         <h3>GBP to Bitcoin Converter</h3>
         <input type="number"value={value} onChange={changeHandler}/>
-        <button onClick={()=>setValue(convert2BTC())}>Convert</button>
+        <button onClick={()=>convert2BTC()}>Convert</button>
         <br/>
-        <p>{currentBTC}</p>
+        <p>{btc}</p>
         </>
     )
 }
