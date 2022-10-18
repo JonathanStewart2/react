a2d2aa301d4234c4de10aa740e9599974b03d36f
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-10-18 15:19:33 +0100

    product manager

--- lifting_state/src/Items.jsx
+++ lifting_state/src/Items.jsx
@@ -1,43 +1,76 @@
-const Items = ({page, addItem}) => {
+import chaos from './chaosList.json';
+import order from './orderList.json';
+import destruction from './destructionList.json';
+import death from './deathList.json';
+
+const Items = ({ page, addItem }) => {
+
 
     if (page === "Chaos") {
-    return (
-        <ul>
-            <li>Archaon <button value="Archaon" onClick={addItem}>Buy</button></li>
-            <li>Darkoath<button value="Darkoath" onClick={addItem}>Buy</button></li>
-            <li>Be'lakor <button value="Be'lakor" onClick={addItem}>Buy</button></li>
-            
-        </ul>
-    )
+        return (
+            <>
+                <ul>
+                    {
+                        chaos.map((item) => (
+                            <li>{item.item} £{item.price}
+                                <button value={item} onClick={() => addItem(item)}>Buy</button>
+                                <br />
+                                <img src={item.image} alt={item.item}/>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </>
+        )
     } else if (page === "Death") {
         return (
-            <ul>
-              <li>Nagash <button value="Nagash" onClick={addItem}>Buy</button></li>
-              <li>Mannfred<button value="Mannfred" onClick={addItem}>Buy</button></li>
-              <li>Arkhan<button value="Arkhan" onClick={addItem}>Buy</button></li>
-            </ul>
+            <>
+                <ul>
+                    {
+                        death.map((item) => (
+                            <li>{item.item} £{item.price}
+                                <button value={item} onClick={() => addItem(item)}>Buy</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </>
         )
     }
     else if (page === "Order") {
         return (
-            <ul>
-                <li>Bastian Carthalos <button value="Bastian Carthalos " onClick={addItem}>Buy</button></li>
-                <li>Knight Relictor<button value="Knight Relictor" onClick={addItem}>Buy</button></li>
-                <li>Naeve Blacktalon<button value="Naeve Blacktalon" onClick={addItem}>Buy</button></li>
-            </ul>
-            )
+            <>
+                <ul>
+                    {
+                        order.map((item) => (
+                            <li>{item.item} £{item.price}
+                                <button value={item} onClick={() => addItem(item)}>Buy</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </>
+        )
     } else if (page === "Destruction") {
         return (
-            <ul>
-                <li>Kragnos <button value="Kragnos" onClick={addItem}>Buy</button></li>
-                <li>Wardokk<button value="Wardokk" onClick={addItem}>Buy</button></li>
-                <li>Loonboss<button value="Loonboss" onClick={addItem}>Buy</button></li>
-            </ul>
+            <>
+                <ul>
+                    {
+                        destruction.map((item) => (
+                            <li>{item.item} £{item.price}
+                                <button value={item} onClick={() => addItem(item)}>Buy</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </>
         )
-    }else {
-        return<>
-            <p>Please select the army to choose from.</p>
-        </>
+    } else {
+        return (
+            <>
+                <p>Please select the army to choose from.</p>
+            </>
+        )
     }
 }
 
--- lifting_state/src/Shop.jsx
+++ lifting_state/src/Shop.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import Items from './Items.jsx';
 import Nav from './Nav.jsx';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Shop = () => {
     const [items, setItems] = useState([]);
     const [basket, setBasket] = useState([]);
-    const [total, setTotal] = useState();
+    const [total, setTotal] = useState([]);
+    const [totalCalc, setTotalCalc] = useState();
     const [page, setPage] = useState('');
 
     const pageHandler = ({target}) => {
@@ -13,16 +17,44 @@ const Shop = () => {
         setPage(target.value);
     }
 
-    const addItem = ({target}) => {
-        let cloneBasket = [...basket, target.value];
+    const sumArray = (array) => {
+        let sum = 0;
+        for (let i=0; i<array.length; i++){
+            sum += array[i];
+        }
+        return sum 
+    };
+
+    const addItem = (item) => {
+        //update Basket
+        let newItem = item.item;
+        let cloneBasket = [...basket];
+        cloneBasket.push(newItem);
         setBasket(cloneBasket);
+        // update Totals array
+        let newCost = item.price;
+        let cloneTotal = total;
+        cloneTotal.push(newCost);
+        setTotal(cloneTotal);
+        //update calculated total
+        setTotalCalc(sumArray(total));
     }
 
+
     const removeItem = (i) => {
+        //remove item from basket
         let cloneBasket = [...basket];
         cloneBasket.splice(i, 1);
         setBasket(cloneBasket);
+        //remove cost of item from total array
+        let cloneTotal = total;
+        cloneTotal.splice(i, 1);
+        setTotal(cloneTotal);
+        // recalculate the total
+        setTotalCalc(sumArray(total));
     }
+
+    
     
     return (
         <>
@@ -34,14 +66,19 @@ const Shop = () => {
             </tr>
             <tr>
                 <td>
-                    <Items page={page} addItem={addItem}/>
+                    <Items page={page} addItem={addItem} />
                 </td>
                 <td>
-                {
+                    <p>
+                    {
                     basket.map((item, i) => (
                     <li>{item}  <button value={item} onClick={()=>removeItem(i)}>Remove</button> </li>)
-                    )
-                }
+                     )
+                     }
+                    </p>
+                    <p>
+                        Total: £{totalCalc}
+                    </p>
                 </td>
             </tr>
         </table>
